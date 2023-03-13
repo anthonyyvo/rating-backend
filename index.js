241c45d09cffd16baed4b7b850374fd3fb08a88b
anthonyyvo <anthonyyvominh@gmail.com>
2023-03-13 11:33:39 +0700

    prover5

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ app.use('/api/users',usersRouter);
 app.get('/', (req, res) => {
     res.send('hello world');
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen('5000', ()=> {
+app.listen(PORT, ()=> {
     console.log('Server is running...')
 })
